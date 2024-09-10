8b7862d2f4c0c7c3942369a811c9ee29888ba413
235003 <235003@KB37-309-C04>
2024-09-10 13:26:51 +0300

    Добавьте файлы проекта.

---
+++ uuurok1/uuurok1.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int bro = 123;
+int main()
+{
+	char w[] = "west";
+	int pluh = 20;
+	string s1 = "hello";
+	char uuu = 's';
+	double d1 = 25;
+	double f1 = 25.6;
+	char nah = 'A';
+	int a992346 = 25;
+	char h = 'H';
+	string dude = "Hello";
+	char b682754 = 'B';
+	int c3 = 18;
+	string s999 = "hi";
+	string t = "lets go";
+	int broo = 120;
+	cout << bro << endl << broo;
+	return 0;
+}
